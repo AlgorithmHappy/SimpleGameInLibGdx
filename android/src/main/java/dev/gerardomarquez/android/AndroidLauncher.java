27077f4fa938e2b4465c6f6c07a740b00c5e5c79
Luis Gerardo Marquez Mendez <lgerardomarquezm@gmail.com>
2025-09-28 19:46:03 -0600

    Se hace el primercommit, todo esta corriendo bien

---
+++ android/src/main/java/dev/gerardomarquez/android/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package dev.gerardomarquez.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import dev.gerardomarquez.Drop;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Drop(), configuration);
+    }
+}
